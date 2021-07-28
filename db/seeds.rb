cb5f13f140e0aaabaa0a10295ab89b1704aa733c
Carla Valdivia <valdivia.carla@gmail.com>
2021-07-28 16:58:59 -0300

    DB and crud actions done

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,19 @@ Recipe.create!(
   rating: "4.75",
   url: "https://www.allrecipes.com/recipe/259091/spiced-mango-lassi/"
 )
+
+Recipe.create!(
+  title: "Milk Chocolate Cheesecake",
+  description: "Top your smooth and creamy Milk Chocolate Cheesecake with generous spoonfuls of whipped cream and fresh strawberries.",
+  prep_time: "4 hrs",
+  rating: "4.5",
+  url: "https://www.allrecipes.com/recipe/246726/milk-chocolate-cheesecake/"
+)
+
+Recipe.create!(
+  title: "Cinnamon Toast",
+  description: "Yummy cinnamon toast makes a great breakfast meal or a snack!",
+  prep_time: "7 mins",
+  rating: "4.58",
+  url: "https://www.allrecipes.com/recipe/52688/cinnamon-toast/"
+)
--- models/recipe.rb
+++ models/recipe.rb
@@ -1,2 +1,4 @@
 class Recipe < ActiveRecord::Base
+  validates :title, presence: true
+  validates :description, presence: true
 end
