6121c87edeb6604bf85ea9dc85cb3e631fe13841
Carla Valdivia <valdivia.carla@gmail.com>
2021-07-29 18:38:01 -0300

    required recipe model in app

--- app.rb
+++ app.rb
@@ -1,16 +1,17 @@
 require_relative "config/application"
+require_relative "models/recipe"
 require "sinatra"
 require "sinatra/reloader"
 require "sinatra/activerecord"
 # require "pry-byebug"
-require "better_errors"
+# require "better_errors"
 require 'nokogiri'
 require 'open-uri'
 
-configure :development do
-  use BetterErrors::Middleware
-  BetterErrors.application_root = File.expand_path('..', __FILE__)
-end
+# configure :development do
+#   use BetterErrors::Middleware
+#   BetterErrors.application_root = File.expand_path('..', __FILE__)
+# end
 
 def scrape_recipe(ingredient)
   @recipes_array = []
