4fbd554cb222f1e4b1c56743aed72b5f7dfe901f
Carla Valdivia <valdivia.carla@gmail.com>
2021-08-07 20:58:27 -0300

    Added rake and pg gems to main list

--- Gemfile
+++ Gemfile
@@ -7,17 +7,11 @@ gem 'sinatra'
 gem 'sinatra-activerecord', :require => 'sinatra/activerecord'
 gem 'activerecord', '5.2.3', :require => 'active_record'
 gem 'rack'
+gem 'pg'
+gem 'rake'
 gem 'require_all'
 
 group :development do
   gem 'sqlite3'
   gem 'pry'
-end
-
-group :production do
-  gem 'pg'
-end
-
-group :development, :production do
-  gem 'rake'
 end
--- config/environment.rb
+++ config/environment.rb
@@ -1,25 +1,29 @@
-ENV['RACK_ENV'] ||= "development"
-
-require 'bundler'
-Bundler.require(:default, ENV['RACK_ENV'])
+require 'bundler/setup'
+ Bundler.require
 
 configure :development do
-  set :database, 'sqlite3:db/development.sqlite'
-end
+  ENV['SINATRA_ENV'] ||= "development"
 
-configure :production do
-  set :database, ENV['DATABASE_URL']
-end
+  require 'bundler/setup'
+  Bundler.require(:default, ENV['SINATRA_ENV'])
+
+  ActiveRecord::Base.establish_connection(
+   :adapter => "sqlite3",
+   :database => "db/#{ENV['SINATRA_ENV']}.sqlite"
+  )
+ end
+
+ configure :production do
+  db = URI.parse(ENV['DATABASE_URL'] || 'postgres:///localhost/mydb')
+
+  ActiveRecord::Base.establish_connection(
+    :adapter  => db.scheme == 'postgres' ? 'postgresql' : db.scheme,
+    :host     => db.host,
+    :username => db.user,
+    :password => db.password,
+    :database => db.path[1..-1],
+    :encoding => 'utf8'
+  )
+ end
 
 require_all 'app'
-
-
-
-
-
-require_all 'app'
-
-ActiveRecord::Base.establish_connection(
-  :adapter => "sqlite3",
-  :database => "db/development.sqlite"
-)
