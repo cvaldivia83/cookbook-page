1b61a72ce2e6948c36333b050482adce922cfccc
Carla Valdivia <valdivia.carla@gmail.com>
2021-08-07 23:24:45 -0300

    seeded db with recipes

---
+++ db/seeds.rb
@@ -0,0 +1,20 @@
+Recipe.create!(
+  title: "Spiced Mango Lassi",
+  description: "Nonfat mango lassi with spices just like in the Indian restaurants. Perfect drink for summer evenings.",
+  prep_time: "10 mins",
+  rating: "4.75",
+)
+
+Recipe.create!(
+  title: "Milk Chocolate Cheesecake",
+  description: "Top your smooth and creamy Milk Chocolate Cheesecake with generous spoonfuls of whipped cream and fresh strawberries.",
+  prep_time: "4 hrs",
+  rating: "4.5",
+)
+
+Recipe.create!(
+  title: "Cinnamon Toast",
+  description: "Yummy cinnamon toast makes a great breakfast meal or a snack!",
+  prep_time: "7 mins",
+  rating: "4.58",
+)
