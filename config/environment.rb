03208efbd90506f3b5d4d67a0601e93e6ad32c52
Carla Valdivia <valdivia.carla@gmail.com>
2024-11-06 17:10:29 -0300

    Updated version of original app

--- config/environment.rb
+++ config/environment.rb
@@ -7,23 +7,34 @@ configure :development do
   require 'bundler/setup'
   Bundler.require(:default, ENV['SINATRA_ENV'])
 
-  ActiveRecord::Base.establish_connection(
-   :adapter => "sqlite3",
-   :database => "db/#{ENV['SINATRA_ENV']}.sqlite"
-  )
+  # ActiveRecord::Base.establish_connection(
+  #  :adapter => "sqlite3",
+  #  :database => "db/#{ENV['SINATRA_ENV']}.sqlite"
+  # )
+
+  set :database, {:adapter =>'sqlite3', :database=>"db/#{ENV['SINATRA_ENV']}.sqlite"}
  end
 
  configure :production do
   db = URI.parse(ENV['DATABASE_URL'] || 'postgres:///localhost/mydb')
 
-  ActiveRecord::Base.establish_connection(
-    :adapter  => db.scheme == 'postgres' ? 'postgresql' : db.scheme,
-    :host     => db.host,
-    :username => db.user,
-    :password => db.password,
-    :database => db.path[1..-1],
-    :encoding => 'utf8'
-  )
+  # ActiveRecord::Base.establish_connection(
+  #   :adapter  => db.scheme == 'postgres' ? 'postgresql' : db.scheme,
+  #   :host     => db.host,
+  #   :username => db.user,
+  #   :password => db.password,
+  #   :database => db.path[1..-1],
+  #   :encoding => 'utf8'
+  # )
+
+  set :database, { 
+    adapter: db.scheme == 'postgres' ? 'postgresql' : db.scheme, 
+    host: db.host,
+    username: db.user,
+    password: db.password,
+    database: db.path[1..-1],
+    encoding: 'utf8'
+  }
  end
 
 require_all 'app'
