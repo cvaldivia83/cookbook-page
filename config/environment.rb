3619774a63bb4b46fb8690c54d7e98fb9f9e4b78
Carla Valdivia <valdivia.carla@gmail.com>
2024-11-06 18:36:27 -0300

    Restored environment config with ActiveRecord::Base.establish_connection

--- config/environment.rb
+++ config/environment.rb
@@ -18,23 +18,23 @@ configure :development do
  configure :production do
   db = URI.parse(ENV['DATABASE_URL'] || 'postgres:///localhost/mydb')
 
-  # ActiveRecord::Base.establish_connection(
-  #   :adapter  => db.scheme == 'postgres' ? 'postgresql' : db.scheme,
-  #   :host     => db.host,
-  #   :username => db.user,
-  #   :password => db.password,
-  #   :database => db.path[1..-1],
-  #   :encoding => 'utf8'
-  # )
+  ActiveRecord::Base.establish_connection(
+    :adapter  => db.scheme == 'postgres' ? 'postgresql' : db.scheme,
+    :host     => db.host,
+    :username => db.user,
+    :password => db.password,
+    :database => db.path[1..-1],
+    :encoding => 'utf8'
+  )
 
-  set :database, { 
-    adapter: db.scheme == 'postgres' ? 'postgresql' : db.scheme, 
-    host: db.host,
-    username: db.user,
-    password: db.password,
-    database: db.path[1..-1],
-    encoding: 'utf8'
-  }
+  # set :database, { 
+  #   adapter: db.scheme == 'postgres' ? 'postgresql' : db.scheme, 
+  #   host: db.host,
+  #   username: db.user,
+  #   password: db.password,
+  #   database: db.path[1..-1],
+  #   encoding: 'utf8'
+  # }
  end
 
 require_all 'app'
